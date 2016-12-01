83a13e58160edf438f9de9cb36a8de63dac569b4
Gaulthier Toussaint <gaulthier.toussaint@etu.unice.fr>
2016-12-01 21:49:46 +0100

    jeu

--- js/MiniJeu.js
+++ js/MiniJeu.js
@@ -2,3 +2,12 @@
  * Created by PierreRainero on 01/12/2016.
  */
 
+var Instructions = ["instru1", "instru2", "instru3"];
+var i=0, l=Instructions.length;
+
+function newInstruction(){
+    if(i<Instructions.length){
+        document.getElementById("instructions").innerHTML = Instructions[i];
+        i++;
+    }
+}
