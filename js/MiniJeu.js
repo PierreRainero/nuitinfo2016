d9c6266134ebebe6113579b8f664c0bda896e688
Pierre Rainero <pierre.rainero@etu.unice.fr>
2016-12-01 21:50:37 +0100

    merge

--- js/MiniJeu.js
+++ js/MiniJeu.js
@@ -1,4 +1,15 @@
 /**
- * Created by PierreRainero on 01/12/2016.
+ * Created by The walking code on 01/12/2016.
  */
 
+var perso = new Object();
+perso.xDecalage = 0;
+perso.yDecalage = 0;
+
+function init(){
+
+}
+
+function sautX(){
+
+}
