dc8dc04808c167a36fc1a58b23be099516c80f9d
Gaulthier Toussaint <gaulthier.toussaint@etu.unice.fr>
2016-12-01 22:26:01 +0100

    a

--- js/MiniJeu.js
+++ js/MiniJeu.js
@@ -2,46 +2,26 @@
  * Created by PierreRainero on 01/12/2016.
  */
 
-var perso = new Object();
-perso.xDecalage = 0;
-perso.yDecalage = 0;
-
-function init(){
-
-}
-
-function sautX(){
-
-}
-
-
 var Instructions = ["instru1", "instru2", "instru3"];
 var i=0, l=Instructions.length;
 
-function newInstruction() {
-    if (i < Instructions.length) {
+function newInstruction(){
+    if(i<Instructions.length){
         document.getElementById("instructions").innerHTML = Instructions[i];
         i++;
     }
 }
 
-function bouger() {
 
+
+function checkAnswer(elem){
+    if(elem.innerHTML != currentQuestion.answer){
+        elem.className += "wrong";
+        tryAgain();
+    }
+    else{
+        elem.className += "right";
+        nextQuestion();
+    }
 }
 
-
-$(document).ready(function(){
-
-    function bougerMapGauche(){
-        $("#fond").animate({left: '5%'}, 100);
-    }
-
-    function bougerMapDroite(){
-        $("#fond").animate({right: '5%'}, 100);
-    }
-
-    function bougerBonhomme() {
-        $("#personnage").animate({top:''})
-    }
-
-})(jQuery);
