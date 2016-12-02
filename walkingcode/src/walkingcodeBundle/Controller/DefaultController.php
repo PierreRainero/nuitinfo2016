3e82c533cb42631c214be4d4559b5cbccc309d08
Sébastien Tosello <sebtosello@gmail.com>
2016-12-02 05:10:36 +0100

    Add grant user page (/promoteUser)

--- walkingcode/src/walkingcodeBundle/Controller/DefaultController.php
+++ walkingcode/src/walkingcodeBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace walkingcodeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -19,5 +20,19 @@ class DefaultController extends Controller
         return $this->render(
             'walkingcodeBundle:Default:index.html.twig',
             array('user_name' => $name));
+    }
+
+    /**
+     * @Route("/promoteUser")
+     */
+    public function promoteUserAction(){
+
+        $user = $this->getUser();
+
+        $userManager = $this->get('fos_user.user_manager');
+        $user->addRole('ROLE_ADMIN');
+        $userManager->updateUser($user);
+
+        return new Response("Role user granted");
     }
 }
