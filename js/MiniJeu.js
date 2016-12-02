4fb5f5fd004ed794be3b39b490fdfdebebfd9c96
Fabien Durando <fabien.durando@etu.unice.fr>
2016-12-02 01:10:47 +0100

    Titeuf marche :)

--- js/MiniJeu.js
+++ js/MiniJeu.js
@@ -4,60 +4,86 @@
 
 (function($) {
     $(document).ready(function(){
-        $("#obstacle1").hide();
-
+        $("#piece").hide();
         var deplacementObstacle=0;
-
+        var nbPourBreak = 1;
         var etapes = 0;
         var nbPieces = 0 ;
+        var animationEnded=false;
 
         function controleurHistoire(){
             switch(etapes){
                 case 0:
                     defiler();
+                    moveElement();
                     etapes++;
                     break;
 
                 case 1:
                     saut();
-                    popPiece();
+                    moveElement();
+                    setTimeout(
+                        function()
+                        {
+                            popPiece();
+                        }, 750);
                     etapes++;
                     break;
 
                 case 2:
                     defiler();
+                    moveElement();
+                    movePiece();
                     etapes++;
                     break;
 
                 case 3:
+                    defiler();
+                    moveElement();
+                    movePiece();
+                    etapes++;
+                    break;
+
+                case 4:
                     if(nbPourBreak==0){
                         etapes++;
                         effacerElementDynamique();
+                        movePiece();
+                        defiler();
                     }
                     else
                         modifieElementDynamique();
                     break;
 
-                case 4:
+                case 5:
                     defiler();
+                    movePiece();
                     etapes++;
                     break;
 
-                case 5:
+                case 6:
                     takePiece();
-                    popElementDynamique();
+                    defiler();
+                    setTimeout(
+                        function()
+                        {
+                            popElementDynamique();
+                        }, 750);
                     etapes=0;
                     break;
             }
-
+            var interval= setInterval(function(){ animationEnded = false; clearInterval(interval); }, 1500);
         }
 
         var Instructions = ["instru1", "instru2", "instru3"];
         var i=0, l=Instructions.length;
 
         $("#reponse1").click(function () {
-            saut();
-            popElementDynamique();
+            if(!animationEnded){
+                animationEnded=true;
+                controleurHistoire();
+            }
+
             if (i < Instructions.length) {
                 document.getElementById("instructions").innerHTML = Instructions[i];
                 i++;
@@ -65,7 +91,7 @@
         })
 
         function defiler(){
-            $("#fond").animate({"left": "-=10%"}, 750);
+            $("#fond").animate({"left": "-=10%"}, 700);
         }
 
 
@@ -75,13 +101,35 @@
         }
 
         function popElementDynamique(){
-            if(deplacementObstacle==0)
-                $("#obstacle1").show();
-            else
-                $("#obstacle1").delay(125).animate({left: "-=10%"}, 750);
-            deplacementObstacle++;
+            nbPourBreak=1;
+            $("#obstacle1").delay(750).show();
         }
 
+        function moveElement() {
+            $("#obstacle1").animate({"left": "-=10%"}, 750);
+        }
+
+        function movePiece() {
+            $("#piece").animate({"left": "-=10%"}, 750);
+        }
+
+        function popPiece() {
+            $("#piece").show();
+        }
+        
+        function effacerElementDynamique() {
+            $("#obstacle1").animate({"left": "+=40%"}, 750);
+        }
+        
+        function modifieElementDynamique() {
+            nbPourBreak--;
+            $("#obstacle1").hide();
+        }
+        
+        function takePiece() {
+            $("#piece").hide();
+            $("#piece").animate({"left": "+=40%"}, 750);
+        }
 
     });
 })(jQuery);
