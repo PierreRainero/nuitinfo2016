99e3dd074a5402720b0389461483fe17cb185e58
Sébastien Tosello <sebtosello@gmail.com>
2016-12-02 06:06:17 +0100

    user get roleuser on registering

---
+++ walkingcode/src/walkingcodeBundle/Controller/RegistrationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace walkingcodeBundle\Controller;
+
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends BaseController
+{
+    /**
+     * Tell the user his account is now confirmed.
+     */
+    public function confirmedAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+
+        $role = "ROLE_ADMIN";
+
+        $user->addRole($role);
+
+        return $this->render('FOSUserBundle:Registration:confirmed.html.twig', array(
+            'user' => $user,
+            'targetUrl' => $this->getTargetUrlFromSession(),
+        ));
+    }
+
+    public function getTarget(){
+        $this->getTargetUrlFromSession();
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getTargetUrlFromSession()
+    {
+        $key = sprintf('_security.%s.target_path', $this->get('security.token_storage')->getToken()->getProviderKey());
+
+        if ($this->get('session')->has($key)) {
+            return $this->get('session')->get($key);
+        }
+    }
+
+}
